daea06a473ec80445ee78b550f0d92b016759e79
kedani <75819419+kedani@users.noreply.github.com>
2021-07-07 23:28:50 +0900

    ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  # resources :pics, only: :index
+  root to: 'pics#index'
+   resources :pics, only: :index
 end
